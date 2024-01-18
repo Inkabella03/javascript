bdae8eec117b31b20d913b4a6f4ad8bb25519110
Inkabella03 <99076322+Inkabella03@users.noreply.github.com>
2024-01-18 08:27:39 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let a ="hallo"
+let b ="world"
+let c = "Inka Bella Nur Aisyah"
+let string1 = "good";
+let string2 = "great";
+let umur = 20
+
+
+let letter = "See You Next Time";
+let lower = letter.toLowerCase();
+
+console.log(a +" "+ b)
+console.log(c)
+console.log(lower);
+console.log(string1.concat(string2));
+console.log(umur == 100) 
+console.log(umur == 20)
+
+
